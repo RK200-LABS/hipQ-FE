61ab0b934afba2d832ec87133673168814bb75df
brianhhy <brianhhy68@gmail.com>
2025-04-24 14:00:49 +0900

    [feat] info 위치 수정 및 다운로드되는 이미지 이름 변경

--- hipQ/components/ui/sheet.jsx
+++ hipQ/components/ui/sheet.jsx
@@ -2,7 +2,8 @@
 
 import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
-import { XIcon } from "lucide-react"
+import { Menu } from "lucide-react"
+
 
 import { cn } from "@/lib/utils"
 
@@ -71,8 +72,8 @@ function SheetContent({
         {...props}>
         {children}
         <SheetPrimitive.Close
-          className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
-          <XIcon className="size-4" />
+          className="data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:outline-hidden disabled:pointer-events-none">
+          <Menu size={30} />
           <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
       </SheetPrimitive.Content>

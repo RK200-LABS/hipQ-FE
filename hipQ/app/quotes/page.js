f817f164751d848fbefb078546daca4cec10dc94
brianhhy <brianhhy68@gmail.com>
2025-05-12 19:08:00 +0900

    [feat] rightquotemaker 모바일 관련 수정(배경색 및 그림자 제거 & 위치 수정)

--- hipQ/app/quotes/page.js
+++ hipQ/app/quotes/page.js
@@ -10,12 +10,12 @@ export default function Quotes() {
       <div className="flex flex-1 pt-[80px] w-full">
         
         {/* LeftContent - 태블릿 이하에서는 숨김 */}
-        <div className="w-1/2 flex justify-center items-center hidden lg:flex">
+        <div className="w-1/2 flex justify-center items-center hidden lg:flex opacity-0 animate-[fade-in_0.5s_ease-out_forwards]">
           <LeftContent />
         </div>
 
         {/* RightContent - 항상 표시됨 */}
-        <div className="w-full lg:w-1/2 flex justify-center items-center">
+        <div className="w-full lg:w-1/2 flex justify-center items-center opacity-0 animate-[fade-in_0.5s_ease-out_0.3s_forwards]">
           <RightContent />
         </div>
         
